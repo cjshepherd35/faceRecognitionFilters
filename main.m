c7077d06da5e0c070b361a6d9c3f1c7eb2ef6382
Colton Shepherd <60142255+cjshepherd35@users.noreply.github.com>
2022-11-17 09:37:14 -0600

    Add files via upload

---
+++ main.m
@@ -0,0 +1,15 @@
+//
+//  main.m
+//  macopencvobjc
+//
+//  Created by Colton Shepherd on 3/23/22.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // Setup code that might create autoreleased objects goes here.
+    }
+    return NSApplicationMain(argc, argv);
+}
